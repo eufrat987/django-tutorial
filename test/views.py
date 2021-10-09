9ff66f97b54d7b916545381d1deb549e58a60606
luk <lukzyw@gmail.com>
2021-10-09 23:10:50 +0000

    upload files

---
+++ test/views.py
@@ -0,0 +1,52 @@
+from django.shortcuts import render 
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import ToDoList
+from .forms import CreateNewList
+
+# Create your views here.
+
+def index(request, id):
+    ls = ToDoList.objects.get(id=id)
+
+    print (request.POST)
+    if request.method == 'POST':
+        print ('saving')
+        if request.POST.get('save'):
+            print ('has')
+            for item in ls.item_set.all():
+                if request.POST.get('c' + str(item.id) ):
+                    item.complete = True
+                else:
+                    item.complete = False
+                item.save()
+             
+
+
+        elif request.POST.get('newItem'):
+            print ('newi')
+            txt = request.POST.get('new')
+            if len(txt) > 2:
+                ls.item_set.create(text=txt, complete=False)
+            else: print ('err')
+            ls.save()
+    
+
+
+    return render(request, 'list.html', {'ls': ls})
+
+def home(request):
+    return render(request, 'home.html', {})
+
+def create(request):
+    if request.method == 'POST':
+        form = CreateNewList(request.POST)
+        if form.is_valid():
+            n = form.cleaned_data['name']
+            t = ToDoList(name = n) 
+            t.save()
+            return HttpResponseRedirect('/%i' % t.id)
+
+    else:
+        form= CreateNewList()
+    return render(request, 'create.html', {'form': form})
+
